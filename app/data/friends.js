2b1b219a42b291e632049cbb072fbb2d3c6aaf7a
Nary Hang <naryhang@gmail.com>
2019-08-19 18:52:33 -0700

    Add files via upload

---
+++ app/data/friends.js
@@ -0,0 +1,57 @@
+
+// DATA
+// Below data will hold all of the reserved tables.
+
+var userData = [
+    {
+      "name": "Pikachu",
+      "photo": "https://i.pinimg.com/originals/cb/33/49/cb3349b86ca661ca61ae9a36d88d70d4.png",
+      "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+      ]
+    },
+    {
+      "name": "Psyduck",
+      "photo": "https://vignette.wikia.nocookie.net/sonicpokemon/images/b/b2/Psyduck_AG_anime.png/revision/latest?cb=20130626041931",
+      "scores":[
+          3,
+          2,
+          2,
+          4,
+          1,
+          1,
+          5,
+          1,
+          3,
+          1
+      ]
+    },
+    {
+      "name": "Squirtle",
+      "photo": "https://miro.medium.com/max/604/1*KuSu6ZTyLAcRDwOsI9ZzZA.png",
+      "scores":[
+          2,
+          1,
+          3,
+          1,
+          5,
+          1,
+          2,
+          1,
+          1,
+          1
+      ]
+    }
+  ];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = userData;
